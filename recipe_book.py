c61f3b4c8373777822ea382e3f3565f5c3739932
Heather Tweedie <rhtweedie12@gmail.com>
2022-05-21 14:50:36 +0100

    Add recipe book and function to search it

---
+++ recipe_book.py
@@ -0,0 +1,42 @@
+recipes = [
+
+    {"name": "Apple crumble",
+     "type": "sweet",
+     "ingredients":["apple", "cinnamon"],
+     "url": "https://www.bbc.co.uk/food/recipes/applecrumble_2971"},
+
+    {"name": "Carrot and coriander soup",
+     "type": "savoury",
+     "ingredients":["carrot", "ground coriander", "fresh coriander"],
+     "url": "https://www.deliciousmagazine.co.uk/recipes/carrot-and-ginger-soup/"},
+
+    {"name": "Carrot and ginger soup",
+     "type": "savoury",
+     "ingredients":["ginger", "carrot"],
+     "url": "https://www.deliciousmagazine.co.uk/recipes/carrot-and-ginger-soup/"},
+
+    {"name": "Ginger brownies",
+     "type": "sweet",
+     "ingredients":["ginger", "dark-chocolate", "cocoa-powder", "egg"],
+     "url": "https://www.foodandspice.com/2010/07/chocolate-brownies-with-fresh-ginger.html"},
+
+    {"name": "Gnocchi alla norma",
+     "type": "savoury",
+     "ingredients":["gnocchi", "aubergine", "basil"],
+     "url": "https://www.bbcgoodfood.com/recipes/gnocchi-alla-norma"},
+
+    {"name": "Gnocchi tomato bake",
+     "type": "savoury",
+     "ingredients":["gnocchi", "pepper", "basil", "mozzerella"],
+     "url": "https://www.bbcgoodfood.com/recipes/gnocchi-tomato-bake"},
+
+    {"name": "Thai green curry",
+     "type": "savoury",
+     "ingredients":["thai green curry paste", "chilli", "butternut squash", "pepper", "coconut milk", "mangetout", "baby corn", "fresh coriander"],
+     "url": "https://cookieandkate.com/vegetarian-chili-recipe/"},
+
+    {"name": "Vegetable chili",
+     "type": "savoury",
+     "ingredients":["pepper", "carrot", "celery", "beans", "fresh coriander"],
+     "url": "https://cookieandkate.com/vegetarian-chili-recipe/"}
+]
---
+++ recipe_search.py
@@ -0,0 +1,41 @@
+"""Function to search a dictionary of recipes for given criteria and return those that match"""
+
+
+from recipe_book import recipes
+
+def recipe_search(recipes):
+
+    repeat = True
+
+    while repeat == True:
+
+        ingredient = input("\nWhich ingredient would you like to search by?")
+        type = input("Would you like to make something sweet or savoury?")
+        suggestions = "\n"
+
+        for recipe in recipes:
+            if ingredient in recipe["ingredients"] and type in recipe["type"]:
+                recipe_name = recipe["name"]
+                recipe_url = recipe["url"]
+                suggestions += f"{recipe_name}: {recipe_url}\n"
+
+        if suggestions == "\n":
+            print("\nYou have no recipes that match those criteria.\n")
+        else:
+            print(f"\n{type.title()} recipes containing {ingredient}: \n{suggestions}")
+
+        valid = False
+        while valid == False:
+            new_search = input("Would you like to carry out another search? [y/n]")
+            if new_search in ('y', 'n'):
+                if new_search == 'y':
+                    valid = True
+                    repeat = True
+                if new_search == 'n':
+                    valid = True
+                    repeat = False
+            else:
+                valid = False
+                print("\nPlease answer with y (yes) or n (no).\n")
+
+recipe_search(recipes)
