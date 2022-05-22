ed0fa3a4e189c52a162eae1501c59e6905857685
Heather Tweedie <rhtweedie12@gmail.com>
2022-05-22 14:00:42 +0100

    Insert spaces before ingredients lists

--- recipe_book.py
+++ recipe_book.py
@@ -2,61 +2,61 @@ recipes = [
 
     {"name": "Apple crumble",
      "type": "sweet",
-     "ingredients":["apple", "cinnamon"],
+     "ingredients": ["apple", "cinnamon"],
      "url": "https://www.bbc.co.uk/food/recipes/applecrumble_2971"},
 
     {"name": "Apple scotch",
      "type": "sweet",
-     "ingredients":["apple", "cinnamon", "egg", "walnuts", "oats"],
+     "ingredients": ["apple", "cinnamon", "egg", "walnuts", "oats"],
      "url": "http://q.geek.nz/recipes/apple_scotch.html"},
 
     {"name": "Carrot and coriander soup",
      "type": "savoury",
-     "ingredients":["carrot", "ground coriander", "fresh coriander"],
+     "ingredients": ["carrot", "ground coriander", "fresh coriander"],
      "url": "https://www.deliciousmagazine.co.uk/recipes/carrot-and-ginger-soup/"},
 
     {"name": "Carrot and ginger soup",
      "type": "savoury",
-     "ingredients":["ginger", "carrot"],
+     "ingredients": ["ginger", "carrot"],
      "url": "https://www.deliciousmagazine.co.uk/recipes/carrot-and-ginger-soup/"},
 
     {"name": "Ginger brownies",
      "type": "sweet",
-     "ingredients":["ginger", "dark-chocolate", "cocoa-powder", "egg"],
+     "ingredients": ["ginger", "dark-chocolate", "cocoa-powder", "egg"],
      "url": "https://www.foodandspice.com/2010/07/chocolate-brownies-with-fresh-ginger.html"},
 
     {"name": "Gnocchi alla norma",
      "type": "savoury",
-     "ingredients":["gnocchi", "aubergine", "basil"],
+     "ingredients": ["gnocchi", "aubergine", "basil"],
      "url": "https://www.bbcgoodfood.com/recipes/gnocchi-alla-norma"},
 
     {"name": "Gnocchi tomato bake",
      "type": "savoury",
-     "ingredients":["gnocchi", "pepper", "basil", "mozzerella"],
+     "ingredients": ["gnocchi", "pepper", "basil", "mozzerella"],
      "url": "https://www.bbcgoodfood.com/recipes/gnocchi-tomato-bake"},
 
     {"name": "Lentil and sweet potato curry",
      "type": "savoury",
-     "ingredients":["lentils", "ginger", "red curry paste", "sweet potato"],
+     "ingredients": ["lentils", "ginger", "red curry paste", "sweet potato"],
      "url": "https://feelgoodfoodie.net/recipe/red-lentil-curry/"},
 
     {"name": "Moroccan couscous",
      "type": "savoury",
-     "ingredients":["pepper", "carrot", "courgette", "couscous", "chickpeas", "almonds", "fresh coriander"],
+     "ingredients": ["pepper", "carrot", "courgette", "couscous", "chickpeas", "almonds", "fresh coriander"],
      "url": "https://www.cookingclassy.com/moroccan-couscous-roasted-vegetables-chick-peas-almonds/"},
 
     {"name": "Thai green curry",
      "type": "savoury",
-     "ingredients":["thai green curry paste", "chilli", "butternut squash", "pepper", "coconut milk", "mangetout", "baby corn", "fresh coriander"],
+     "ingredients": ["thai green curry paste", "chilli", "butternut squash", "pepper", "coconut milk", "mangetout", "baby corn", "fresh coriander"],
      "url": "https://www.bbcgoodfood.com/recipes/vegetarian-thai-green-curry"},
 
     {"name": "Tomato and halloumi bake",
      "type": "savoury",
-     "ingredients":["halloumi", "aubergine", "courgette"],
+     "ingredients": ["halloumi", "aubergine", "courgette"],
      "url": "https://www.tamingtwins.com/grilled-halloumi-cheese-bake-recipe/"},
 
     {"name": "Vegetable chili",
      "type": "savoury",
-     "ingredients":["pepper", "carrot", "celery", "beans", "fresh coriander"],
+     "ingredients": ["pepper", "carrot", "celery", "beans", "fresh coriander"],
      "url": "https://cookieandkate.com/vegetarian-chili-recipe/"}
 ]
