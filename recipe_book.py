615c12be7be6470f35d437ea00d43612e564f4a3
Heather Tweedie <rhtweedie12@gmail.com>
2022-05-23 15:39:40 +0100

    Change category to tags and lists

--- recipe_book.py
+++ recipe_book.py
@@ -5,22 +5,22 @@ recipes = [
     #########
 
     {"name": "Apple crumble",
-     "category": "sweet",
+     "tags": ["sweet"],
      "ingredients": ["apple", "cinnamon"],
      "location": "https://www.bbc.co.uk/food/recipes/applecrumble_2971"},
 
     {"name": "Apple scotch",
-     "category": "sweet",
+     "tags": ["sweet"],
      "ingredients": ["apple", "cinnamon", "egg", "walnuts", "oats"],
      "location": "http://q.geek.nz/recipes/apple_scotch.html"},
 
     {"name": "Ginger brownies",
-     "category": "sweet",
+     "tags": ["sweet"],
      "ingredients": ["ginger", "dark-chocolate", "cocoa-powder", "egg"],
      "location": "https://www.foodandspice.com/2010/07/chocolate-brownies-with-fresh-ginger.html"},
 
     {"name": "Oatmeal raisin cookies",
-     "category": "sweet",
+     "tags": ["sweet"],
      "ingredients": ["egg", "black treacle", "raisins", "oats", "walnuts"],
      "location": "https://sallysbakingaddiction.com/soft-chewy-oatmeal-raisin-cookies/"},
 
@@ -30,12 +30,12 @@ recipes = [
     ########
 
     {"name": "Carrot and coriander soup",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["carrot", "ground coriander", "fresh coriander"],
      "location": "https://www.deliciousmagazine.co.uk/recipes/carrot-and-ginger-soup/"},
 
     {"name": "Carrot and ginger soup",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["ginger", "carrot"],
      "location": "https://www.deliciousmagazine.co.uk/recipes/carrot-and-ginger-soup/"},
 
@@ -45,37 +45,37 @@ recipes = [
     ########
 
     {"name": "Gnocchi alla norma",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["gnocchi", "aubergine", "basil"],
      "location": "https://www.bbcgoodfood.com/recipes/gnocchi-alla-norma"},
 
     {"name": "Gnocchi tomato bake",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["gnocchi", "pepper", "basil", "mozzerella"],
      "location": "https://www.bbcgoodfood.com/recipes/gnocchi-tomato-bake"},
 
     {"name": "Lentil and sweet potato curry",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["lentils", "ginger", "red curry paste", "sweet potato"],
      "location": "https://feelgoodfoodie.net/recipe/red-lentil-curry/"},
 
     {"name": "Moroccan couscous",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["pepper", "carrot", "courgette", "couscous", "chickpeas", "almonds", "fresh coriander"],
      "location": "https://www.cookingclassy.com/moroccan-couscous-roasted-vegetables-chick-peas-almonds/"},
 
     {"name": "Thai green curry",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["thai green curry paste", "chilli", "butternut squash", "pepper", "coconut milk", "mangetout", "baby corn", "fresh coriander"],
      "location": "https://www.bbcgoodfood.com/recipes/vegetarian-thai-green-curry"},
 
     {"name": "Tomato and halloumi bake",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["halloumi", "aubergine", "courgette"],
      "location": "https://www.tamingtwins.com/grilled-halloumi-cheese-bake-recipe/"},
 
     {"name": "Vegetable chili",
-     "category": "savoury",
+     "tags": ["savoury"],
      "ingredients": ["pepper", "carrot", "celery", "beans", "fresh coriander"],
      "location": "https://cookieandkate.com/vegetarian-chili-recipe/"}
 ]
