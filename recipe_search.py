578a56a664c0b8bd234919bf0128f5d79b60c6fb
Heather Tweedie <rhtweedie12@gmail.com>
2022-05-26 16:28:38 +0100

    Scrape and print recipes from available websites

--- recipe_search.py
+++ recipe_search.py
@@ -15,6 +15,7 @@ limitations under the License.
 """
 
 
+from recipe_scrapers import scrape_me
 import yaml
 with open("recipes.yaml", "r") as stream:
     recipes = yaml.safe_load(stream)
@@ -29,7 +30,7 @@ def get_all(recipes, category):
 
 
 def get_input(category):
-    """Get input as set from user and validate"""
+    """Get input from user and validate"""
     while True:
         all = get_all(recipes, category)
         user_input = set((input(f"\nWhich {category} would you like to search by? ")).split(", "))
@@ -39,6 +40,25 @@ def get_input(category):
             return user_input
 
 
+def format_recipe(recipe_to_format):
+    scraper = scrape_me(recipe_to_format["location"])
+    print(f"\n--{scraper.title()}--")
+    print("\nIngredients:\n")
+    ingredients = "\n-".join(scraper.ingredients())
+    print(f"-{ingredients}")
+    print("\nInstructions:\n")
+    print(scraper.instructions())
+
+
+def get_valid_y_n(prompt, correction):
+    while True:
+        user_input = input(prompt)
+        if user_input not in ("y", "n"):
+            print(correction)
+        else:
+            return user_input
+        
+
 def recipe_search(recipes):
     """Search a list of recipes for given criteria and return those that match"""
 
@@ -47,22 +67,34 @@ def recipe_search(recipes):
         ingredients = get_input("ingredients")
         tags = get_input("tags")
 
-        suggestions = [f"{recipe['name']}: {recipe['location']}" for recipe in recipes
-            if ingredients.issubset(recipe["ingredients"]) and tags.issubset(recipe["tags"])]
+        suggestions = []
+        count = 0
+        for recipe in recipes:
+            if ingredients.issubset(recipe["ingredients"]) and tags.issubset(recipe["tags"]):
+                count += 1
+                suggestions.append(recipe)
 
         if not suggestions:
             print("\nYou have no recipes that match those criteria.\n")
         else:
             print(f"\n{tags} recipes containing {ingredients}:\n")
-            print("\n".join(suggestions))
+            print("\n".join((f"{suggestions.index(recipe) + 1}. {recipe['name']}: {recipe['location']} (scrapable: {recipe['scrapable']})") for recipe in suggestions))
 
         while True:
-            new_search = input("\nWould you like to carry out another search? [y/n] ")
-            if new_search not in ("y", "n"):
-                print("\nPlease answer with y (yes) or n (no).\n")
+            view_recipe = get_valid_y_n(("\nWould you like to view a recipe? "),
+                                        ("\nPlease answer with 'y' (yes) or 'n' (no)."))
+            if view_recipe == "y":
+                recipe_index = int(input("Which recipe would you like to view? ")) - 1
+                recipe_to_view = suggestions[recipe_index]
+                if recipe_to_view["scrapable"] == True:
+                    print(format_recipe(recipe_to_view))
+                else:
+                    print("That recipe is not available to view.")
             else:
                 break
 
+        new_search = get_valid_y_n(("\nWould you like to carry out another search? [y/n] "), 
+                                    ("\nPlease answer with 'y' (yes) or 'n' (no).\n"))
         if new_search == 'n':
             return
 
