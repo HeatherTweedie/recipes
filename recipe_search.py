63a57fe0db8f0dc812154c687bcf070db7322403
Heather Tweedie <rhtweedie12@gmail.com>
2022-05-29 22:30:39 +0100

    Fix output messages

--- recipe_search.py
+++ recipe_search.py
@@ -79,21 +79,24 @@ def recipe_search(recipes):
                 if ingredients.issubset(recipe["ingredients"]):
                     count += 1
                     suggestions.append(recipe)
+                    message = f"All recipes containing {ingredients}:\n"
         elif ingredients == "none":
             for recipe in recipes:
                 if tags.issubset(recipe["tags"]):
                     count += 1
                     suggestions.append(recipe)
+                    message = f"\nAll {tags} recipes:\n"
         else:
             for recipe in recipes:
                 if ingredients.issubset(recipe["ingredients"]) and tags.issubset(recipe["tags"]):
                     count += 1
                     suggestions.append(recipe)
+                    message = f"\n{tags} recipes containing {ingredients}:\n"
 
         if not suggestions:
             print("\nYou have no recipes that match those criteria.\n")
         else:
-            print(f"\n{tags} recipes containing {ingredients}:\n")
+            print(message)
             print("\n".join((f"{suggestions.index(recipe) + 1}. {recipe['name']}: {recipe['location']} (scrapable: {recipe['scrapable']})") for recipe in suggestions))
 
         while True:
