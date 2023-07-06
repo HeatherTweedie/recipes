b1232e6a42f653e20f4f8b299ea42087c46c8e7c
Heather Tweedie <rhtweedie12@gmail.com>
2023-07-06 22:42:04 +0100

    Make String lists just Strings

--- src/rht/recipeSearch/Recipe.java
+++ src/rht/recipeSearch/Recipe.java
@@ -1,26 +1,32 @@
 public class Recipe {
 
+    private final int id;
     private final String name;
-    private final String[] tags;
-    private final String[] ingredients;
+    private final String tags;
+    private final String ingredients;
     private final String location;
 
-    public Recipe(String name, String[] tags, String[] ingredients, String location) {
+    public Recipe(int id, String name, String tags, String ingredients, String location) {
+        this.id = id;
         this.name = name;
         this.tags = tags;
         this.ingredients = ingredients;
         this.location = location;
     }
 
+    public int getID() {
+        return this.id;
+    }
+
     public String getName() {
         return this.name;
     }
 
-    public String[] getTags() {
+    public String getTags() {
         return this.tags;
     }
 
-    public String[] getIngredients() {
+    public String getIngredients() {
         return this.ingredients;
     }
 
